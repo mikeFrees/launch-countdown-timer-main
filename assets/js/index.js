89845c2fdf364a06c7ddde6568d71e21b85e2dbe
Mike Frees <mike_frees@hotmail.com>
2023-09-19 23:09:44 +0200

    start of timer function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,63 @@
-const timeLeft = fetch('https://mikes-launch-countdown.netlify.app/assets/data/time.json')
-    .then((response) => response.json());
+/*const timeLeft = fetch('https://mikes-launch-countdown.netlify.app/assets/data/time.json')
+    .then((response) => response.json()); */
+
+let timeLeft = {
+    "days": "14",
+    "hours": "00",
+    "minutes": "00",
+    "seconds": "1000"
+};
+
+setInterval(tick(), 1000);
+
+function tick() {
+    switch (timeLeft) {
+        case (timeLeft.days === 0 && timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds === 0):
+          resetTime();
+          break;
+        case (timeLeft.days !== 0 && timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds === 0):
+            dayChange();
+            break;
+        case (timeLeft.hours !== 0 && timeLeft.minutes === 0 && timeLeft.seconds === 0):
+            hourChange();
+            break;
+        case (timeLeft.minutes !== 0 && timeLeft.seconds === 0):
+            minuteChange();
+            break;
+        case (timeLeft.seconds !== 0):
+            secondChange();
+            break;
+        default:
+          console.log(`bug`);
+      }
+    return;
+}
+
+function dayChange(timeLeft) {
+    timeLeft.days -= 1;
+    timeLeft.hours = 23;
+    timeLeft.minutes = 59;
+    timeLeft.seconds = 59;
+}
+
+function hourChange(timeLeft) {
+    timeLeft.hours -= 1;
+    timeLeft.minutes = 59;
+    timeLeft.seconds = 59;
+}
+
+function minuteChange(timeLeft) {
+    timeLeft.minutes -= 1;
+    timeLeft.seconds = 59;
+}
+
+function secondChange(timeLeft) {
+    timeLeft.seconds -= 1;
+}
+
+function resetTime(timeLeft) {
+    timeLeft.days = 14;
+    timeLeft.hours = 0;
+    timeLeft.minutes = 0;
+    timeLeft.seconds = 0;
+}
